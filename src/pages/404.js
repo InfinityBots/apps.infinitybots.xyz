f1483f7252a5888c39b26148291ad932f0489436
Infinity Bot List <74225528+InfinityBots@users.noreply.github.com>
2021-02-26 21:23:01 -0700

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,5 +3,5 @@ import React from "react"
 export default () =>
   <div>
     <h1 style={{ color: 'white' }}>404 - NOT FOUND</h1>
-    <p style={{ color: 'white' }}>You just hit a route that doesn't exist... the sadness.</p>
+    <p style={{ color: 'white' }}>It seems like you have traveled to far into the Deep Dark Web.</p>
   </div>
