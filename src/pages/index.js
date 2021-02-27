ae00a6e14130e90b78d5ac13c348dff69b9413df
Infinity Bot List <74225528+InfinityBots@users.noreply.github.com>
2021-02-26 21:46:21 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,12 @@ export default class Index extends React.Component {
   render() {
     return (
       <div>
-        <h1 style={{ color: 'white' }}>Hello Everyone!</h1>
         <p style={{ color: 'white' }}>
           If you're interested in Aplying to be a member of our team, Reporting a bug or anything else you're in the right place, Please choose from one of the Forms Below
         </p>
         <ul>
-          <li style={{ color: 'white' }}><Link to="/bugs/" style={{ color: '#00B0E1' }}>Bug Reports</Link></li>
-          <li style={{ color: 'white' }}><Link to="/application/" style={{ color: '#00B0E1' }}>Staff/Team Applications</Link></li>
-          <li style={{ color: 'white' }}><Link to="/request/" style={{ color: '#00B0E1' }}>Feature Request</Link></li>
+          <li style={{ color: '#8A6AFD' }}><Link to="/appeals/" style={{ color: '#00B0E1' }}>Bug Reports</Link></li>
+          <li style={{ color: '#8A6AFD' }}><Link to="/application/" style={{ color: '#00B0E1' }}>Staff/Team Applications</Link></li>
         </ul>
       </div>
     );
