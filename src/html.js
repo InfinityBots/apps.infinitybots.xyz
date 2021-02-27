36fb45d9c6b0caf4cf13432cd3304d8c871d4b31
Infinity Bot List <74225528+InfinityBots@users.noreply.github.com>
2021-02-26 20:58:38 -0700

    Update html.js

--- src/html.js
+++ src/html.js
@@ -31,6 +31,7 @@ export default class HTML extends React.Component {
           />
           {this.props.headComponents}
           {css}
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         </head>
         <body>
           <div
