6940cccab1babbad6f0b06264d1e5a17e9704fb9
Infinity Bot List <74225528+InfinityBots@users.noreply.github.com>
2021-02-26 21:58:26 -0700

    Update application.jsx

--- src/pages/application.jsx
+++ src/pages/application.jsx
@@ -35,7 +35,7 @@ export default class Contact extends React.Component {
   render() {
     return (
       <div>
-        <h1 style={{ color: 'white' }}>Staff/Team Applications</h1>
+        <h1 style={{ color: 'white' }}>Staff Application</h1>
         <form
           name="application"
           method="post"
