a0ebfee6cdd1eb59382f22f1d868029664ed5e0e
Infinity Bot List <74225528+InfinityBots@users.noreply.github.com>
2021-05-26 22:20:06 -0600

    Update thanks.jsx

--- src/pages/thanks.jsx
+++ src/pages/thanks.jsx
@@ -3,6 +3,6 @@ import React from "react";
 export default () => (
   <div>
     <h1 style={{ color: 'white' }}>Form Submitted!</h1>
-    <p style={{ color: 'white' }}>Thanks for Submitting a form, If a Member of our Staff Team have any questions they will be in touch! </p>
+    <p style={{ color: 'white' }}>Thanks for Submitting a Application, If a Member of our Staff Team have any questions they will be in touch! </p>
   </div>
 );
