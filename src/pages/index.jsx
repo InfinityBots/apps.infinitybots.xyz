8e430cee14aa50b5844320d6e5be969200a381bf
Infinity Bot List <74225528+InfinityBots@users.noreply.github.com>
2021-05-26 22:35:26 -0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,7 +12,7 @@ export default class Index extends React.Component {
           If you're interested in Aplying to be a member of our team, Appeal a ban or anything else you're in the right place, Please choose from one of the Forms Below
         </p>
         <ul>
-          <li style={{ color: '#8A6AFD' }}><Link to="https://appeals.botlist.site" style={{ color: '#00B0E1' }}>Ban Appeals</Link></li>
+          <li style={{ color: '#8A6AFD' }}><Link href="https://appeals.botlist.site" style={{ color: '#00B0E1' }}>Ban Appeals</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/staff/" style={{ color: '#00B0E1' }}>Staff Applications</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/devs/" style={{ color: '#00B0E1' }}>Dev Applications</Link></li>
         </ul>
