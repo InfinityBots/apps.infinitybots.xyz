c3919dde14cb0dfa94ff84341aa5f295b2943959
Infinity Bot List <74225528+InfinityBots@users.noreply.github.com>
2021-06-14 13:26:53 -0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,13 +6,15 @@ import '../css/custom.tachyons.css';
 
 export default class Index extends React.Component {
   render() {
+   let url="https://appeals.botlist.site/";
     return (
       <div>
         <p style={{ color: 'white' }}>
           If you're interested in Aplying to be a member of our team, Appeal a ban or anything else you're in the right place, Please choose from one of the Forms Below
         </p>
         <ul>
-          <li style={{ color: '#8A6AFD' }}><Link href="https://appeals.botlist.site" style={{ color: '#00B0E1' }}>Ban Appeals</Link></li>
+          <li style={{ color: '#8A6AFD' }}><a href={url} style={{ color: '#00B0E1' }}>Ban Appeals</a></li>
+          <li style={{ color: '#8A6AFD' }}><Link to="/devs/" style={{ color: '#00B0E1' }}>Dev Applications</Link></li>
           <li style={{ color: '#8A6AFD' }}><Link to="/devs/" style={{ color: '#00B0E1' }}>Dev Applications</Link></li>
         </ul>
       </div>
